057b3c1f5714f714f5913b55a83d86f8f7421334
Sahana-sonu <93938850+Sahana-sonu@users.noreply.github.com>
2024-03-01 11:02:39 +0530

    Create CalculatorTest.java

---
+++ src/test/java/com/example/javamavenjunithelloworld/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.sample;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(1, calculator.subtract(4, 3));
+    }
+}
